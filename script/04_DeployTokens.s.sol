25e4e9dafe4466a2cb1ce779059d94cf18356e4e
gimersettle <gimer.cervera@settlenetwork.com>
2025-06-25 22:10:23 -0600

    add aggregator Mock

--- script/04_DeployTokens.s.sol
+++ script/04_DeployTokens.s.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.28;
 
 import { Script } from "forge-std/Script.sol";
-import { MockERC20 } from "src/MockERC20.sol";
+import { MockERC20 } from "src/utils/MockERC20.sol";
 
 import { console } from "forge-std/console.sol";
 
